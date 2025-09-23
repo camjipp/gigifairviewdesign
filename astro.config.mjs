6801284023e34e91a0ab0c141983439fece9d98a
Cameron Jipp <cameronjipp@MacBook-Pro-2.local>
2025-09-23 11:32:02 -0400

    Configure base path for GitHub Pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,7 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+  base: '/gigifairviewdesign/',
   vite: {
     plugins: [tailwindcss()]
   }
