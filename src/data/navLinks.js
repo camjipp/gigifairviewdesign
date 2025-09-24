e45e17780102f218ab9645157bf5a32ebbb918cf
Cameron Jipp <cameronjipp@MacBook-Pro-2.local>
2025-09-24 03:22:14 -0400

    Attempt to fix anchor link 404 by prepending BASE_URL

--- src/data/navLinks.js
+++ src/data/navLinks.js
@@ -3,7 +3,7 @@ const BASE_URL = import.meta.env.BASE_URL;
 export const links = [
   { href: `${BASE_URL}`, label: "Home" },
   { href: `${BASE_URL}collections`, label: "Collections" },
-  { href: "/#about", label: "About" },
-  { href: "/#testimonials", label: "Reviews" },
+  { href: `${BASE_URL}#about`, label: "About" },
+  { href: `${BASE_URL}#testimonials`, label: "Reviews" },
   { href: `${BASE_URL}contact`, label: "Contact" },
 ];
